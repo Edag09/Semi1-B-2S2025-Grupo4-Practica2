76e8c9bad7d7a8cec161dcb03ef955c0b45fe146
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2025-10-10 14:06:44 -0600

    UPDATE
    Register Conectado

---
+++ recipeboxcloud/src/Components/Config/apiRoutes.js
@@ -0,0 +1,20 @@
+const API_ROUTES={
+
+    AUTH: {
+        LOGIN: 'http://localhost:8080/auth/login',
+        REGISTER: 'http://localhost:8080/auth/register',
+    },
+
+    AZURE_IMAGES:{
+        UPLOAD_PROFILE: '/azure/upload',
+        UPLOAD_RECIPE: '/azure/upload-recipe-image'
+    },
+
+    RECIPES:{
+        CREATE: '/recipes',
+        GET_ALL: '/recipes',
+        GET_BY_ID: (id) => `/recipes/${id}`,
+    }
+}
+
+export default API_ROUTES;
