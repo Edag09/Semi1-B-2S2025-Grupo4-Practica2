6fe39fd665ac7c938896150c98378aa489850495
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2025-10-16 15:32:46 -0600

    HTTPS

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ const uploadRoutes = require('./routes/upload.routes.js');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8443;
 
 // ===== Middlewares =====
 app.use(cors());
